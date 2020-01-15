7d646c09743a1b0a7673af0c1803f3c2e492a6b7
biagioiorio <biorio06@gmail.com>
2020-01-15 17:29:50 +0100

    aggiunta la navigazione tra le pagine ranking e profilo

---
+++ www/js/navigation.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('a').click(function(e){
+        e.preventDefault();
+        $("#content").load($(this).attr('href'));
+    }); 
+});
+    
+
+
+// // Listen for fragment identifier value changes.
+// window.addEventListener("hashchange", function (){
+
+//     // Get a reference to the "content" div.
+//     var contentDiv = document.getElementById("content");
+  
+//     // Set the "content" div to contain the current hash value.
+//     contentDiv.innerHTML = location.hash;
+// });
